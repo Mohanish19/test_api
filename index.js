4dd20fc22bfa0f470b28d21e29ebc9ba0f612723
Mohanish <manushevde95@gmail.com>
2023-07-06 16:57:33 +0530

    Modified code

--- index.js
+++ index.js
@@ -1,27 +1,14 @@
 const express = require('express');
+
 const app = express();
 
+require('dotenv').config();
 
-require("dotenv").config();
-let port = process.env.PORT;
+const port = process.env.PORT;
 
-app.use('/healthcheck', require('./routes/healthchecker'))
-
-app.use(express.json())
-
-app.get('/', (request,response) => {
-    response.json({data: 'ok'});
-})
-
-app.get('/test', (request,response) => {
-    response.json({data: 'test ok'});
-})
-
-app.post('/save/:name' , (request,response) => {
-    console.log(request.params, request.body);
-    response.json({data: request.params})
-})
-
+app.get('/health', (req, res) => {
+  res.sendStatus(200);
+});
 app.listen(port, () => {
-    console.log("Server has started");
-})
+  console.log('Server has started');
+});
--- routes/healthchecker.js
+++ routes/healthchecker.js
@@ -1,17 +1,19 @@
-const express = require("express");
+const express = require('express');
+
 const router = express.Router({});
-router.get('/', async(_res,res,_next) => {
-    const healthcheck = {
-        uptime: process.uptime(),
-        message: 'OK',
-        timestamp: Date.now()
-    };
-    try {
-        res.send(healthcheck);
-    } catch (error){
-        healthcheck.message = error;
-        res.status(503).send();
-    }
+// eslint-disable-next-line no-unused-vars
+router.get('/', async (_res, res, _next) => {
+  const healthcheck = {
+    uptime: process.uptime(),
+    message: 'OK',
+    timestamp: Date.now(),
+  };
+  try {
+    res.send(healthcheck);
+  } catch (error) {
+    healthcheck.message = error;
+    res.status(503).send();
+  }
 });
 
 module.exports = router;
