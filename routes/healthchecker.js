8d39bb06e1e796ff5155fe0135f4612644a125d4
Mohanish <manushevde95@gmail.com>
2023-07-06 16:53:03 +0530

    Modified healthcheck Adding through new branch

--- routes/healthchecker.js
+++ routes/healthchecker.js
@@ -1,17 +1,13 @@
-const express = require("express");
-const router = express.Router({});
-router.get('/', async(_res,res,_next) => {
-    const healthcheck = {
-        uptime: process.uptime(),
-        message: 'OK',
-        timestamp: Date.now()
-    };
-    try {
-        res.send(healthcheck);
-    } catch (error){
-        healthcheck.message = error;
-        res.status(503).send();
-    }
+const express = require('express');
+
+const app = express();
+
+app.get('/health', (req, res) => {
+  const healthStatus = {
+    status: 'ok',
+    version: '1.0.0',
+  };
+  res.send.json(healthStatus);
 });
 
-module.exports = router;
+module.exports = app;
