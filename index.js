9cceb1854353488f3a2cb38d4866e7de89f7b791
Mohanish <manushevde95@gmail.com>
2023-07-10 13:58:27 +0530

    included base64 into index.js

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const port = process.env.PORT;
 app.get('/health', (req, res) => {
   res.sendStatus(200);
 });
+
+app.get('/encode', (req, res) => {
+  const { text } = req.query;
+
+  if (!text) {
+    return res.status(400).json({ error: 'Text parameter is required.' });
+  }
+
+  const encodedText = Buffer.from(text).toString('base64');
+
+  return res.json({ encodedText });
+});
+
 app.listen(port, () => {
   console.log('Server has started');
 });
